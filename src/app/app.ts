f0c8465c7c422c7d5bd8529339bc84c33445bb26
jose-aluno <josealuno70@gmail.com>
2025-11-29 01:05:32 +0000

    feat: estiliza hero section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./core/navbar/navbar";
+import { HeroSection } from './shared/hero-section/hero-section';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, HeroSection],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
