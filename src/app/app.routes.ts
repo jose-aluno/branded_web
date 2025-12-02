e00fe3d4c7bd7595ad0fddeee5eea4273d3fb2d5
José Henrique Falsarella Lima Oliveira <josealuno70@gmail.com>
2025-12-02 17:04:49 -0300

    feat: estiliza tela de login/criar conta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+import { Register } from './features/account/register/register';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'products', component: ListaProdutos},
+    {path: 'register', component: Register},
     {path: '**', redirectTo: ''}
 ];
