74e8a454534c709284e462ce0091a5b3e24ef6ad
José Henrique Falsarella Lima Oliveira <josealuno70@gmail.com>
2025-12-03 20:50:21 -0300

    feat: define lógica de carrossel dos cards de destaque na home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { HeroSection } from '../../shared/hero-section/hero-section';
 import { Footer } from '../../core/footer/footer';
 import { HighlightCard } from "../produtos/highlight-card/highlight-card";
 import { RouterLink } from "@angular/router";
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ProductService } from '../../services/product/product-service';
 
 @Component({
   selector: 'app-home',
@@ -11,4 +13,45 @@ import { RouterLink } from "@angular/router";
   styleUrl: './home.css',
 })
 export class Home {
+  private productService = inject(ProductService);
+
+  private allProducts = toSignal(this.productService.findAll(), { initialValue: [] });
+  
+  recentProducts = computed(() => {
+    return [...this.allProducts()].reverse().slice(0, 10);
+  });
+
+  currentIndex = signal(0);
+  itemsPerPage = 3;
+
+  maxIndex = computed(() => {
+    return Math.max(0, this.recentProducts().length - this.itemsPerPage);
+  });
+
+  visibleProducts = computed(() => {
+    const start = this.currentIndex();
+    return this.recentProducts().slice(start, start + this.itemsPerPage);
+  });
+
+  next() {
+    const current = this.currentIndex();
+    const max = this.maxIndex();
+
+    if (current < max) {
+      this.currentIndex.update(v => v + 1);
+    } else {
+      this.currentIndex.set(0);
+    }
+  }
+
+  prev() {
+    const current = this.currentIndex();
+    const max = this.maxIndex();
+
+    if (current > 0) {
+      this.currentIndex.update(v => v - 1);
+    } else {
+      this.currentIndex.set(max);
+    }
+  }
 }
--- src/app/features/produtos/highlight-card/highlight-card.ts
+++ src/app/features/produtos/highlight-card/highlight-card.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Product } from '../../../models/product';
 
 @Component({
   selector: 'highlight-card',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './highlight-card.css',
 })
 export class HighlightCard {
-
+  product = input.required<Product>();
 }
