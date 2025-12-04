c8e051af93fdcb2ba0a09bc76e461a7d8fc2fc19
José Henrique Falsarella Lima Oliveira <josealuno70@gmail.com>
2025-12-03 22:17:30 -0300

    feat: cria componentes filhos de profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,21 @@ import { Home } from './features/home/home';
 import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 import { Register } from './features/account/register/register';
 import { Profile } from './features/account/profile/profile';
+import { Data } from './features/account/profile/data/data';
+import { Address } from './features/account/profile/address/address';
+import { Orders } from './features/account/profile/orders/orders';
+import { CreateProduct } from './features/produtos/create-product/create-product';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'products', component: ListaProdutos},
     {path: 'register', component: Register},
-    {path: 'profile', component: Profile},
+    {path: 'profile', component: Profile, children: [
+      { path: '', redirectTo: 'data', pathMatch: 'full' },
+      { path: 'data', component: Data },
+      { path: 'address', component: Address },
+      { path: 'orders', component: Orders },
+      { path: 'createProduct', component: CreateProduct },
+    ]},
     {path: '**', redirectTo: ''}
 ];
