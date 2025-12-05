964e8160bd75be972f261f9dfb4c034647f549e2
Jonas Pinheiro <jonas.pinheiro@aluno.ifsp.edu.br>
2025-12-05 20:38:26 -0300

    feat: implementa lógica de abrir carrinho

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,13 +6,20 @@ import { Footer } from './core/footer/footer';
 import { CardProduto } from './features/produtos/card-produto/card-produto';
 import { HighlightCard } from "./features/produtos/highlight-card/highlight-card";
 import { CartItem } from "./features/produtos/cart-item/cart-item";
+import { Cart } from './features/cart/cart';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Cart],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('branded_web');
+
+  showCart = signal(false);
+
+  toggleCart() {
+    this.showCart.update(currentValue => !currentValue);
+  }
 }
--- src/app/core/navbar/navbar.ts
+++ src/app/core/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { RouterLink } from "@angular/router";
 
 @Component({
@@ -8,5 +8,9 @@ import { RouterLink } from "@angular/router";
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  public cartClicked = output<void>();
 
+  openCart() {
+    this.cartClicked.emit();
+  }
 }
